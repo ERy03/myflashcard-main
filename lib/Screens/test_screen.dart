9ec8660e2c602c60edb0593bd3d4c96d80508b7f
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-18 18:17:37 +0900

    test screen set up

--- lib/Screens/test_screen.dart
+++ lib/Screens/test_screen.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+enum TestStatus {
+  BEFORE_START,
+  SHOW_QUESTION,
+  SHOW_ANSWER,
+  FINISHED,
+}
+
 class TestScreen extends StatefulWidget {
   final bool isIncludedMemorizedWords;
 
@@ -14,6 +21,15 @@ class _TestScreenState extends State<TestScreen> {
   Widget build(BuildContext context) {
     var isIncludedMemorizedWords = widget.isIncludedMemorizedWords;
     return Scaffold(
+      appBar: AppBar(
+        title: Text("かくにんテスト"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("You pressed me"), //TODO
+        child: Icon(Icons.skip_next),
+        tooltip: "次に進む",
+      ),
       body: Center(
         child: Text("TestScreen: $isIncludedMemorizedWords"),
       ),
